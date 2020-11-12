dc6684c4b23f87514107484ac94c5eabf7216473
73 <73@users.noreply.github.com>
2020-11-12 10:06:06 +0100

    fix: wraped lines not spaced

--- linespaces.py
+++ linespaces.py
@@ -2,6 +2,7 @@
 # Authors: Hamad Al Marri <hamad.s.almarri@gmail.com>
 # Copyright: Copyright (C) 2020 Hamad Al Marri
 # Website: https://github.com/hamadmarri
+# https://askubuntu.com/questions/547353/gedit-how-to-increase-spaces-between-lines
 
 from gi.repository import GObject, Gtk, Gedit
 
@@ -17,15 +18,14 @@ class LineSpacing(GObject.Object, Gedit.ViewActivatable):
 		if self.view:
 			self.view.set_pixels_below_lines(self.pixels)
 			self.view.set_pixels_above_lines(self.pixels)
+			self.view.set_pixels_inside_wrap(self.pixels)
 
 
 	def do_deactivate(self):
 		if self.view:
 			self.view.set_pixels_below_lines(0)
 			self.view.set_pixels_above_lines(0)
+			self.view.set_pixels_inside_wrap(0)
 
 
 
-
-        
-        
